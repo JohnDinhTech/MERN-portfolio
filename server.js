724ab1572bd9fb56107305ab537e0611635cdcef
John Dinh <johnduydinh@gmail.com>
2020-02-27 07:50:31 -0500

    able to upload photos to database

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const connectDB = require("./db");
+const { connectDB, connectGridFs } = require("./db");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Connect to DB
 connectDB();
 
 // Init Middleware
@@ -12,6 +13,36 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
+
+const runAsyncServer = async () => {
+	try {
+		const { gfs, upload } = await connectGridFs();
+		app.post("/upload", upload.single("image"), (req, res) => {
+			res.redirect("/");
+		});
+		app.get("/image/:filename", async (req, res) => {
+			const { filename } = req.params;
+			try {
+				const file = await gfs.find({ filename }).toArray();
+
+				if (file.length === 0 || !file) {
+					res.status(404).json({ err: "This file does not exist" });
+					res.redirect("/");
+				} else {
+					gfs.openDownloadStreamByName(filename).pipe(res);
+				}
+			} catch (error) {
+				res.status(404).json({ err: "This file does not exist" });
+				console.error(error.message);
+			}
+		});
+	} catch (error) {
+		console.error(error.message);
+		res.status(400).json({ err: error.message });
+	}
+};
+
+runAsyncServer();
 
 // Use this to register users
 // app.use("/api/users", require("./routes/api/users"));
